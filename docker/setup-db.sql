5e8ef13e1376fb838b7a5a5b617de53d2b5cf4b3
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-05-08 14:46:15 +0200

    feat: Insert default values into the database.

--- docker/setup-db.sql
+++ docker/setup-db.sql
@@ -1,5 +1,15 @@
 create database if not exists emu;
 
+create table if not exists emu.measurement
+(
+    measurementId       int not null,
+    measurementValue double,
+    timeMillis          float,
+    measurementSeriesId int not null,
+    primary key (measurementId, measurementSeriesId),
+    foreign key (measurementSeriesId) references measurementSeries (measurementSeriesId)
+);
+
 create table if not exists emu.measurementSeries
 (
     measurementSeriesId int not null,
@@ -9,12 +19,4 @@ create table if not exists emu.measurementSeries
     primary key (measurementSeriesId)
 );
 
-create table if not exists emu.measurement
-(
-    measurementId       int not null,
-    measurementValue double,
-    timeMillis          float,
-    measurementSeriesId int not null,
-    primary key (measurementId),
-    foreign key (measurementSeriesId) references measurementSeries (measurementSeriesId)
-);
+insert into emu.measurementSeries(measurementSeriesId, timeInterval, consumer, measurementSize) values (1, 5000, "Smartphone", "Leistung");
