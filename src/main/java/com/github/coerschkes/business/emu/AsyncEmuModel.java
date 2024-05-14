9f5b85187d6f048a76b5e0adf37b9636dad7fea3
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-05-14 10:53:00 +0200

    feat: Implement the first version of the async emu model.

--- src/main/java/com/github/coerschkes/business/emu/AsyncEmuModel.java
+++ src/main/java/com/github/coerschkes/business/emu/AsyncEmuModel.java
@@ -1,4 +1,25 @@
 package com.github.coerschkes.business.emu;
 
+import com.github.coerschkes.business.model.Measurement;
+
+import java.util.concurrent.CompletableFuture;
+
 public class AsyncEmuModel {
+    private static AsyncEmuModel modelInstance;
+    private final AsyncEmuRepository repository;
+
+    public static AsyncEmuModel getInstance() {
+        if (modelInstance == null) {
+            modelInstance = new AsyncEmuModel();
+        }
+        return modelInstance;
+    }
+
+    private AsyncEmuModel() {
+        repository = new AsyncEmuRemoteRepository();
+    }
+
+    public CompletableFuture<Measurement> readMeasurement() {
+        return this.repository.readMeasurement();
+    }
 }
